4801345b87ffda1bf42138da315e6e0ae20b5c7f
Maria João <mariajoao.tlp@gmail.com>
2022-11-15 20:08:54 +0000

    Database connection

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
+    dotenv.config({ path: './config/config.env' });
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/database');
+    connectDB();
 
 
 const app = express();
 
+    if( process.env.NODE_ENV === 'development' ){
+        app.use(morgan('dev'));
+    }
 
+
+const PORT = process.env.PORT || 5000;
 app.listen(
-    5000,
-    console.log("server runing on port 5000")
+    PORT,
+    console.log("server runing on port", PORT)
 );
