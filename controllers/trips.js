8aa8a923433137f8374c202306b732bbb5ab213e
Maria João <mariajoao.tlp@gmail.com>
2022-11-17 19:17:15 +0000

    Implementing asyncHandler

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,83 +1,64 @@
 const Trip = require('../models/Trip');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/asyncHandler'); 
 
 // @desc      Get all trips
 // @route     GET /api/v1/trips
 // @access    Public
 
-exports.getTrips = async (req, res, next) => {
-    try{
-        const trips = await Trip.find();
-        res.status(200).json( {success: true, data: trips} );
-    } catch( err ){
-        next( err );
-    }
-};
+exports.getTrips = asyncHandler ( async (req, res, next) => {
+    const trips = await Trip.find();
+    res.status(200).json( {success: true, data: trips} );
+});
 
 
 // @desc      Get single trip
 // @route     GET /api/v1/trips/:id
 // @access    Public
 
-exports.getTrip = async ( req, res, next ) => {
-    try{
-        const trip = await Trip.findById(req.params.id);
-        if( !trip ){
-            return next( new ErrorResponse(`Trip not found with id ${req.params.id}`, 404) );
-        }
-        res.status(200).json( {success: true, data: trip} );
-    } catch( err ){
-        next( err );
+exports.getTrip = asyncHandler( async( req, res, next ) => {
+    const trip = await Trip.findById(req.params.id);
+    if( !trip ){
+        return next( new ErrorResponse(`Trip not found with id ${req.params.id}`, 404) );
     }
-};
+    res.status(200).json( {success: true, data: trip} ); 
+});
 
 
 // @desc      Create new trip
 // @route     POST /api/v1/trips
 // @access    Private
 
-exports.createTrip = async (req, res, next) => {
-    try {
-        const trip = await Trip.create(req.body);
-        res.status(201).json( {success: true, data: trip} );
-    } catch( err ){
-        next( err );
-    }
-};
+exports.createTrip = asyncHandler( async( req, res, next ) => {
+    const trip = await Trip.create(req.body);
+    res.status(201).json( {success: true, data: trip} );
+});
 
 
 // @desc      Update trip
 // @route     PUT /api/v1/trips/:id
 // @access    Private
 
-exports.updateTrip = async( req, res, next ) => {
-    try{
-        const trip = await Trip.findByIdAndUpdate( req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        } );
-        if( !trip ){
-            return next( new ErrorResponse(`Trip not found with id ${req.params.id}`, 404) );
-        }
-        res.status(200).json( {success: true, data: trip} );
-    } catch( err ){
-        next( err );
+exports.updateTrip = asyncHandler( async( req, res, next ) => {
+    const trip = await Trip.findByIdAndUpdate( req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    } );
+    if( !trip ){
+        return next( new ErrorResponse(`Trip not found with id ${req.params.id}`, 404) );
     }
-};
+    res.status(200).json( {success: true, data: trip} );
+} );
 
 
 // @desc      Delete trip
 // @route     DELETE /api/v1/trips/:id
 // @access    Private
 
-exports.deleteTrip = async (req, res, next) => {
-    try{
-        const trip = await Trip.findByIdAndDelete(req.params.id);
+exports.deleteTrip = asyncHandler( async( req, res, next ) => {
+    const trip = await Trip.findByIdAndDelete(req.params.id);
         if( !trip ){
             return next( new ErrorResponse(`Trip not found with id ${req.params.id}`, 404) );
         }
-        res.status(200).json( {success: true, data: trip} ); //retorna o elemento removido
-    } catch( err ){
-        next( err );
-    }
-};
+        res.status(200).json( {success: true, data: trip} );
+} );
---
+++ middleware/asyncHandler.js
@@ -0,0 +1,5 @@
+// 13
+const asyncHandler = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next); 
+  
+module.exports = asyncHandler;
