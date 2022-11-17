69aa6055dc623ed5913d2250bb8e09e448c05adc
Maria João <mariajoao.tlp@gmail.com>
2022-11-17 19:38:31 +0000

    Advanced filtering and pagination on get Trips

--- controllers/trips.js
+++ controllers/trips.js
@@ -7,8 +7,61 @@ const asyncHandler = require('../middleware/asyncHandler');
 // @access    Public
 
 exports.getTrips = asyncHandler ( async (req, res, next) => {
-    const trips = await Trip.find();
-    res.status(200).json( {success: true, data: trips} );
+    let query;
+    const reqQuery = { ...req.query };
+    const removeFields = ['select', 'sort', 'page', 'limit'];
+    removeFields.forEach(param => delete reqQuery[param]); 
+    let queryStr = JSON.stringify(reqQuery); 
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`); 
+    query = Trip.find(JSON.parse(queryStr));
+    
+    // Select Fields
+    if (req.query.select) {
+      const fields = req.query.select.split(',').join(' '); 
+      console.log(fields);
+      query = query.select(fields);
+    }
+
+    // Sort 
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy); 
+    } else {
+      query = query.sort('-createdAt'); 
+    }
+
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1; 
+    const limit = parseInt(req.query.limit, 10) || 2; 
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Trip.countDocuments();
+    query = query.skip(startIndex).limit(limit); 
+    const trips = await query;
+
+    // Pagination result
+    const pagination = {};
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit
+      };
+    }
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit
+      };
+    }
+
+    res
+      .status(200)
+      .json({
+        success: true,
+        count: trips.length,
+        pagination,
+        data: trips
+      });
 });
 
 
