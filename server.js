a93e45d244c79aaaf75788997fd52012c609550b
Maria João <mariajoao.tlp@gmail.com>
2022-11-25 08:56:56 +0000

    API security

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const connectDB = require('./config/database');
     connectDB();
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 
 //Route files
@@ -27,6 +33,16 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10min
+    max: 100
+});
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 
 
 //Mount routes
